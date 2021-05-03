0d8b3977c2d2c00688d313cea79807e3d8703801
ffionda <fiorella.fionda@cern.ch>
2021-05-03 13:31:45 +0200

    updated launch script 30-50%

--- Cent30to50/launchScript.sh
+++ Cent30to50/launchScript.sh
@@ -1,3 +1,6 @@
+### new
+./computeLikeFitMVA.sh 2.60 2.80 3.20 3.52
+
 #./computeLikeFitMVA.sh 2.50 2.80 3.20 3.60
 #./computeLikeFitMVA.sh 2.50 2.75 3.16 3.60
 #./computeLikeFitMVA.sh 2.50 2.85 3.16 3.60
@@ -5,7 +8,7 @@
 #./computeLikeFitMVA.sh 2.50 2.85 3.25 3.60
 
 #./computeLikeFitMVAptintegr.sh 2.50 2.80 3.20 3.60
-./computeLikeFitMVAptintegr.sh 2.50 2.75 3.16 3.60
-./computeLikeFitMVAptintegr.sh 2.50 2.85 3.16 3.60
-./computeLikeFitMVAptintegr.sh 2.50 2.75 3.25 3.60
-./computeLikeFitMVAptintegr.sh 2.50 2.85 3.25 3.60
+#./computeLikeFitMVAptintegr.sh 2.50 2.75 3.16 3.60
+#./computeLikeFitMVAptintegr.sh 2.50 2.85 3.16 3.60
+#./computeLikeFitMVAptintegr.sh 2.50 2.75 3.25 3.60
+#./computeLikeFitMVAptintegr.sh 2.50 2.85 3.25 3.60
